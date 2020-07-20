054f90eb2d765ca135c656072601c14605b4d52f
ianbautista <ianfbautista@gmail.com>
2020-07-20 14:39:53 -0700

    fixed error no rendering on screen

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React from "react";
+import { useRouteMatch, useHistory } from "react-router-dom";
 
-const MovieList = props => {
-  return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
-}
+const MovieList = (props) => {
+	return (
+		<div className="movie-list">
+			{props.movies.map((movie) => (
+				<MovieDetails key={movie.id} movie={movie} />
+			))}
+		</div>
+	);
+};
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
-  );
+	const { path } = useRouteMatch();
+	const history = useHistory();
+
+	const { title, director, metascore } = movie;
+	return (
+		<div className="movie-card">
+			<h2>{title}</h2>
+			<div className="movie-director">
+				Director: <em>{director}</em>
+			</div>
+			<div className="movie-metascore">
+				Metascore: <strong>{metascore}</strong>
+			</div>
+		</div>
+	);
 }
 
 export default MovieList;
