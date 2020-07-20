a4474228a5ee8c616f515a512b5df08ce0c25d42
ianbautista <ianfbautista@gmail.com>
2020-07-20 13:55:53 -0700

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
-  const [movieList, setMovieList] = useState([]);
+	const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+	const [movieList, setMovieList] = useState([]);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovieList(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+	useEffect(() => {
+		const getMovies = () => {
+			axios
+				.get("http://localhost:5000/api/movies")
+				.then((response) => {
+					setMovieList(response.data);
+				})
+				.catch((error) => {
+					console.error("Server Error", error);
+				});
+		};
+		getMovies();
+	}, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+	const addToSavedList = (id) => {
+		// This is stretch. Prevent the same movie from being "saved" more than once
+	};
 
-  return (
-    <div>
-      <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList
+				list={
+					[
+						/* This is stretch */
+					]
+				}
+			/>
+			{/* <div>Replace this Div with your Routes</div> */}
+			<Switch>
+				<Route path="/movies/:id">
+					<Movie />
+				</Route>
+
+				<Route path="/">
+					<MovieList movies={movieList} />
+				</Route>
+			</Switch>
+		</div>
+	);
 };
 
 export default App;
